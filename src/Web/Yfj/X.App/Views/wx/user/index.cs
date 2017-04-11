8b0d8c8f1a89067b22c7bc715c83f8de9aec540d
Li Xinyue <lxy_2008focus@126.com>
2017-04-11 11:55:58 +0800

    对未支付订单增加了取消订单操作

--- src/Web/Yfj/X.App/Views/wx/user/index.cs
+++ src/Web/Yfj/X.App/Views/wx/user/index.cs
@@ -3,16 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace X.App.Views.wx.user
-{
-    public class index : _wx
-    {
-        protected override void InitView()
-        {
+namespace X.App.Views.wx.user {
+    public class index : _wx {
+        protected override void InitView() {
             base.InitView();
             dict.Add("ac", cu.x_address.Count());
             dict.Add("o1", cu.x_order.Count(o => o.status == 1));
-            dict.Add("o3", cu.x_order.Count(o => o.status == 3));
+            dict.Add("o3", cu.x_order.Count(o => (o.status == 2||o.status == 3  )));
             dict.Add("o4", cu.x_order.Count(o => o.status == 4));
         }
     }
