5be415f65bb0f31ca59b5fd38b99eb29cc4eb8c2
Li Xinyue <lxy_2008focus@126.com>
2017-04-12 13:48:12 +0800

    增加了订单退款管理的后台实现

---
+++ src/Web/Yfj/X.App/Apis/mgr/refund/refuse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using X.Core.Cache;
+using X.Data;
+using X.Web;
+using X.Web.Com;
+
+namespace X.App.Apis.mgr.refund
+{
+    public class refuse : xmg
+    {
+        public int id { get; set; }
+        public string reason { get; set; }
+
+        protected override XResp Execute()
+        {
+            var od = DB.x_refund.FirstOrDefault(o => o.refund_id == id);
+
+            if (od == null) throw new XExcep("T订单不存在");
+            if (od.status != 1) throw new XExcep("T订单状态不正确");
+
+            od.status = 3;
+            od.aname = mg.name;
+            od.atime = DateTime.Now;//????服务器时间
+            od.reason = reason;
+
+            SubmitDBChanges();
+
+            return new XResp();
+        }
+    }
+}
