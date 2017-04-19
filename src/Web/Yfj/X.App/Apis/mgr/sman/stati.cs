3824dcf23b270b0f6c4f24b6eb98e180f8d27c0a
Li Xinyue <lxy_2008focus@126.com>
2017-04-19 10:44:48 +0800

    增加了业务员业绩查看的日期选择

--- src/Web/Yfj/X.App/Apis/mgr/sman/stati.cs
+++ src/Web/Yfj/X.App/Apis/mgr/sman/stati.cs
@@ -9,12 +9,18 @@ namespace X.App.Apis.mgr.sman {
     public class stati : xmg {
         public int page { get; set; }
         public int limit { get; set; }
+        public DateTime ctime { get; set; }
+        public DateTime etime { get; set; }
         public string key { get; set; }
 
         protected override XResp Execute() {
             var r = new Resp_List();
             r.page = page;
-
+            DateTime flag = new DateTime(1970, 1, 1);
+            if(ctime<flag)
+                ctime=flag;
+            if(etime<flag||etime<ctime||etime>DateTime.Now)
+                etime=DateTime.Now;
             var q = from d in GetDictList("user.sman", "0")
                     select d.value;
 
@@ -22,14 +28,15 @@ namespace X.App.Apis.mgr.sman {
                       where q.Contains(d.x_user.invter + "")
                       select d;
             //var us = DB.x_user.Where(u => q.Contains(u.invter + "")).Select(u => (long?)u.user_id);
-            var list = ods.Where(o=>o.city==mg.city).GroupBy(o => o.x_user.invter).Select(g => new {
+
+            var list = ods.Where(o => o.city == mg.city && o.ctime > (ctime) && o.ctime < etime).GroupBy(o => o.x_user.invter).Select(g => new {
                 name = GetDictName("user.sman", g.Key),
                 member_count = DB.x_user.Where(u => u.invter == g.Key).Count(),
                 order_total = g.Count(),
                 order_amount = g.Sum(order => order.yf_amount),
                 finish_amount = ods.Where(od => od.status == 5 && od.x_user.invter == g.Key)//&& od.x_refund.First(o=>o.order_id==od.order_id) != null)
                                     .Sum(order => order.yf_amount)
-            }).OrderByDescending(o=>o.finish_amount);
+            }).OrderByDescending(o => o.finish_amount);
             //list;
 
 
@@ -44,5 +51,16 @@ namespace X.App.Apis.mgr.sman {
             return r;
         }
 
+        private Boolean searchCondition(x_order o) {
+            if (ctime == null && etime == null)//00
+                return o.city == mg.city;
+            //else !00=01/10/11
+            else if (ctime == null)
+                return o.city == mg.city && o.ctime > ctime;
+            else if (etime == null)
+                return o.city == mg.city && o.ctime < etime;
+            else
+                return o.city == mg.city && o.ctime > ctime && o.ctime < etime;
+        }
     }
 }
