561bd2ac7f2a7f77d4ba0a84ffff568c8cffaf01
Admin <Admin@ADMIN-PC>
2017-04-26 17:33:59 +0800

    增加了充值功能，短信接口，但都有一些问题有待调整

---
+++ src/Web/Yfj/X.App/Apis/mgr/deposit/accept.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using X.Core.Cache;
+using X.Data;
+using X.Web;
+using X.Web.Com;
+using X.App.Com;
+
+namespace X.App.Apis.mgr.deposit {
+    public class accept : xmg {
+        public int id { get; set; }
+        public Decimal ramount { get; set; }
+
+        public String remark { get; set; }
+        protected override int powercode {
+            get {
+                return 1;
+            }
+        }
+
+        protected override XResp Execute() {
+            var od = DB.x_refund.FirstOrDefault(o => o.refund_id == id);
+
+            if (od == null) throw new XExcep("T订单不存在");
+            if (od.status != 1) throw new XExcep("T订单状态不正确");
+            od.remark = remark;
+            od.ramount = ramount;
+            var response = Wx.Pay.Refund(cfg.wx_appid, cfg.wx_mch_id, od.x_order.x_user.wx_opid, od.x_order.no, od.refund_id.ToString(),
+                (int)(od.x_order.yf_amount * 100) + "", ((int)(ramount * 100)).ToString(), cfg.wx_paykey, cfg.wx_certpath);
+            if(response.return_code.Equals("FAIL"))
+                throw new XExcep("T退款失败,原因: " + response.return_msg);
+            if (response.result_code.Equals("SUCCESS")) {
+                od.status = 2;
+                od.aname = mg.name;
+                od.atime = DateTime.Now;
+                
+                SubmitDBChanges();
+            } else
+                throw new XExcep("T退款失败,原因: " + response.err_code_des);
+
+            return new XResp();
+        }
+    }
+}
