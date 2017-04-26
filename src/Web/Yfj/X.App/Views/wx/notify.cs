ea3a63f067e731aed9a568c69fb70c8e6e06ea81
Li Xinyue <lxy_2008focus@126.com>
2017-04-26 11:29:38 +0800

    快没电了

--- src/Web/Yfj/X.App/Views/wx/notify.cs
+++ src/Web/Yfj/X.App/Views/wx/notify.cs
@@ -6,28 +6,22 @@ using X.App.Com;
 using X.Core.Plugin;
 using X.Core.Utility;
 
-namespace X.App.Views.wx
-{
-    public class notify : _wx
-    {
+namespace X.App.Views.wx {
+    public class notify : _wx {
         [ParmsAttr(name = "订单号", req = true)]
         public string no { get; set; }
-        protected override bool nd_user
-        {
-            get
-            {
+        public int tp { get; set; }
+        protected override bool nd_user {
+            get {
                 return false;
             }
         }
-        protected override string GetParmNames
-        {
-            get
-            {
-                return "no";
+        protected override string GetParmNames {
+            get {
+                return "tp-no";
             }
         }
-        public override byte[] GetResponse()
-        {
+        public override byte[] GetResponse() {
             GetPageParms();
             cfg = Config.LoadConfig();
             var ntxml = string.Empty;
@@ -37,48 +31,54 @@ namespace X.App.Views.wx
             ntxml = ntxml.Replace("xml", "Ntxml");
             var nt = Serialize.FormXml<Wx.Pay.Ntxml>(ntxml);
 
-            if (!Wx.Pay.ValidNotify(nt, cfg.wx_mch_id, cfg.wx_appid, cfg.wx_paykey))
-            {
+            if (!Wx.Pay.ValidNotify(nt, cfg.wx_mch_id, cfg.wx_appid, cfg.wx_paykey)) {
                 Loger.Info("回调验证失败，地址：" + Context.Request.RawUrl + "，参数：" + ntxml);
                 return null;
             }
 
             var no = dict.GetString("no");
             if (string.IsNullOrEmpty(no)) return null;
-            if (nt.out_trade_no != no)
-            {
+            if (nt.out_trade_no != no) {
                 Loger.Info("订单号不正确，地址：" + Context.Request.RawUrl + "，提交单号：" + nt.out_trade_no);
                 return null;
             }
-
-            var order = DB.x_order.SingleOrDefault(o => o.no == no);
-
             var okxml = @"<xml>
                             <return_code><![CDATA[SUCCESS]]></return_code>
                             <return_msg><![CDATA[OK]]></return_msg>
                         </xml>";
 
-            if (order == null)
-            {
-                Loger.Info("订单不存在，订单号：" + no);
-                return Encoding.UTF8.GetBytes(okxml);
-            }
+            if (tp != 2) {
+                var order = DB.x_order.SingleOrDefault(o => o.no == no);
 
-            if (order.status != 1 || order.pay_way != 1)
-            {
-                Loger.Info("订单确认失败，订单号：" + no);
-                return Encoding.UTF8.GetBytes(okxml);
-            }
-            else
-            {
-                order.wx_no = nt.transaction_id;
-                order.pay_time = DateTime.Now;
-                order.pay_amount = order.yf_amount;
-                order.status = 2;
-            }
 
-            SubmitDBChanges();
 
+                if (order == null) {
+                    Loger.Info("订单不存在，订单号：" + no);
+                    return Encoding.UTF8.GetBytes(okxml);
+                }
+
+                if (order.status != 1 || order.pay_way != 1) {
+                    Loger.Info("订单确认失败，订单号：" + no);
+                    return Encoding.UTF8.GetBytes(okxml);
+                } else {
+                    order.wx_no = nt.transaction_id;
+                    order.pay_time = DateTime.Now;
+                    order.pay_amount = order.yf_amount;
+                    order.status = 2;
+                }
+
+                SubmitDBChanges();
+            } else {
+                long depositNo = long.Parse(no);
+                var depositItem = DB.x_charge.SingleOrDefault(o => o.charge_id == depositNo);
+                if (depositItem == null) {
+                    Loger.Info("充值记录不存在，订单号：" + no);
+                    return Encoding.UTF8.GetBytes(okxml);
+                }
+                depositItem.ctime = DateTime.Now;
+                depositItem.result = "SUCCESS";
+                SubmitDBChanges();
+            }
             return Encoding.UTF8.GetBytes(okxml);
         }
     }
